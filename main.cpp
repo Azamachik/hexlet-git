e0dbe86f85a2b2a8a0f72f40c691178503d69f86
Azamat Karimov <azamat-karimov-05@mail.ru>
2024-08-03 23:51:18 +0300

    Commit

--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
-#include "task1.h"
-#include "task2.h"
-using namespace task1;
-using namespace task2;
-int main() {
-    int task_number;
-    bool exit {false};
-    while (!exit) {
-    cout << "Select a number of task (1, 2) or 0 (to exit): ";
-    cin >> task_number;
-    switch (task_number) {
-        case 1:
-            task259();
-            break;
-        case 2:
-            task375();
-            break;
-        case 0:
-            exit = true;
-            break;
-        default:
-            cout << "Try again!";
-            break;
-    }
-    }
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int size = 10, w = 8;
+int res = size++ * w + 20;
+    printf("%d", res);
     return 0;
 }
