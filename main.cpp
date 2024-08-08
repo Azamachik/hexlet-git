081bb982763ae93b540a8b68c3d89f6bbc176afd
Azamat Karimov <azamat-karimov-05@mail.ru>
2024-08-08 18:50:41 +0300

    Revert "new commit"
    This reverts commit d50d1da1423c5faea62005b1c8e7fc5e34e38827.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <math.h>
 int main(void)
 {
-    for (int i = 0; i < 10; i++) {
-        printf("num = %d\n", i);
-    }
+    int size = 10, w = 8;
+int res = size++ * w + 20;
+    printf("%d", res);
+    return 0;
 }
